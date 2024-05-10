fefa0e04e80f2ad0646f94e15a98716063007bcb
Beqa Tvildiani <btvildia@c2r10s11.42wolfsburg.de>
2024-05-11 00:47:42 +0200

    fix_leaks

--- fix_leaks.sh
+++ fix_leaks.sh
@@ -1,7 +1,13 @@
-cp ~/ft_destructor/fix_leaks.py .
-python3 fix_leaks.py
-rm -rf fix_leaks.py
-cp -r ~/ft_destructor/ft_destructor .
-cd ft_destructor
-make
-cd ..
+file="Makefile"
+
+if [ -e "$file" ]; then
+  cp ~/ft_destructor/fix_leaks.py .
+  python3 fix_leaks.py
+  rm -rf fix_leaks.py
+  cp -r ~/ft_destructor/ft_destructor .
+  cd ft_destructor
+  make
+  cd ..
+else
+  echo "Makefile not found"
+fi
